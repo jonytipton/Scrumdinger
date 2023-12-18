6e7ed1ec65614a0d55d0a10aa95d430c965149d2
Jonathan Tipton <nature_explorer98@outlook.com>
2023-12-17 19:38:17 -0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jonathan Tipton on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
