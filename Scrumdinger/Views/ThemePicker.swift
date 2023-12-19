759e1878aaaaeac09d94c8d908692f28b074fce9
Jonathan Tipton <nature_explorer98@outlook.com>
2023-12-18 18:30:16 -0700

    Section completed

---
+++ Scrumdinger/Views/ThemePicker.swift
@@ -0,0 +1,26 @@
+//
+//  Picker.swift
+//  Scrumdinger
+//
+//  Created by Jonathan Tipton on 12/18/23.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                    ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+        .pickerStyle(.navigationLink)
+    }
+}
+
+#Preview {
+    ThemePicker(selection: .constant(.periwinkle))
+}
