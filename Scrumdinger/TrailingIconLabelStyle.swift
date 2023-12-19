e0de66e6e5c251f748f43345b9772504c754b4f8
Jonathan Tipton <nature_explorer98@outlook.com>
2023-12-18 17:02:50 -0700

    Completed section (fixed files that were reference from ~)

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Jonathan Tipton on 12/17/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
